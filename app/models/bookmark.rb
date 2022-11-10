f78a34a219107ad6aa44937fe3d81ae95bb2df6e
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-11-10 13:25:45 +0900

    finished validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
+  # Validates the MOVIE(id) with the uniqueness of the list_id combo.
+  validates :movie, uniqueness: { scope: :list_id }
 end
